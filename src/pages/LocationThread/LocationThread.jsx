b6cfef49c3ede5c756be06ff3cff9e1f884ed0d3
caitlinds <caitlindsnell@gmail.com>
2023-03-29 13:05:29 -0700

    heroku

--- src/pages/LocationThread/LocationThread.jsx
+++ src/pages/LocationThread/LocationThread.jsx
@@ -1,10 +1,20 @@
 import { checkToken } from '../../utilities/users-service';
+import './LocationThread.css';
 
 export default function LocationThread() {
   
   return (
     <>
-      <h1>Location Thread</h1>
+      <main className="LocationThread">
+        <aside className="checkedUsers">
+          <h1>Checked In Users</h1>
+          <h2>Log Out</h2>
+        </aside>
+        <hr/>
+        <section>
+          <h1>Posts</h1>
+        </section>
+      </main>
     </>
   );
 }
