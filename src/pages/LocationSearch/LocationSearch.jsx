aa23d53b1838aa30cb3d0e4e1db64ebd3d880991
caitlinds <caitlindsnell@gmail.com>
2023-03-29 17:03:21 -0700

    search pg changes

--- src/pages/LocationSearch/LocationSearch.jsx
+++ src/pages/LocationSearch/LocationSearch.jsx
@@ -1,5 +1,16 @@
 export default function LocationSearch() {
+  function handleClick() {
+    alert('clicked');
+  }
   return (
-    <h1>Location Search</h1>
+    <>
+    <main>
+      <h1 className="LocationSearch">Location Search</h1>
+      <input type="text" name="" value="Find a Location"></input>
+      <div class="center-align">
+          <button type="submit" onClick={handleClick}>Search</button>
+      </div>
+    </main>
+    </>
   );
 }
